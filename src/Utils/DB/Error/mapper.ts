34be67b4b7ea95da6372c4fa0900da8d9e3215eb
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2022-11-23 22:02:44 -0800

    Postgres and MySQL error mapper refactored

--- src/Utils/DB/Error/mapper.ts
+++ src/Utils/DB/Error/mapper.ts
@@ -1,21 +1,21 @@
-import { POSTGRES_ERROR_CODES, MYSQL_ERROR_CODES } from '../Enums';
+import { POSTGRES_ERROR_CODES, MYSQL_ERROR_CODES, ERRORS } from '../Enums';
 
-export const postgresErrorMapper = (code: string, itemName?: string, err?: any): string => {
+export const postgresErrorMapper = (code: string): string => {
   switch (code) {
     case POSTGRES_ERROR_CODES.TABLE_NOT_FOUND:
-      return itemName !== undefined ? `Table "${itemName}" not found` : 'Table not found';
+      return ERRORS.DATABASE.TABLE_NOT_FOUND;
     case POSTGRES_ERROR_CODES.COLUMN_NOT_FOUND:
-      return err !== undefined ? err.message : 'Column not found';
+      return ERRORS.DATABASE.COLUMN_NOT_FOUND;
     default:
-      return 'Unknown error';
+      return ERRORS.DATABASE.UNKNOWN;
   }
 };
 
-export const mysqlErrorMapper = (code: number, itemName?: string, err?: any): string => {
+export const mysqlErrorMapper = (code: number): string => {
   switch (code) {
     case MYSQL_ERROR_CODES.COLUMN_NOT_FOUND:
-      return err !== undefined ? err.message : 'Column not found';
+      return ERRORS.DATABASE.COLUMN_NOT_FOUND;
     default:
-      return 'Unknown error';
+      return ERRORS.DATABASE.UNKNOWN;
   }
 };
